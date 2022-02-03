71d596ec98ada9cdd1dfdac62d9c064acc877665
twillis90 <t.willis9290@gmail.com>
2022-02-03 18:09:31 -0500

    filled in about us and styled

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -1,14 +1,44 @@
+import aaronPic from '../../images/aaron-profile.png'
+import landonPic from '../../images/dolph.jpg'
+import mattPic from '../../images/jason.jpg'
+import tjPic from '../../images/cartoon-tj.png'
+import tommyPic from '../../images/terry.webp'
+
 export default function About() {
     return `
     <div class="page__container">
-      <div>
-        //   <img class='page-image' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.newsbuzau.ro%2Fimages%2Fstories%2F16.03.2019%2Fmatematician-Grigori_Perelman.jpg&f=1&nofb=1' alt='grigs'/>
-          <h2>meet the team</h2>
-          <article>
-            <h4>The man the myth the legend</h4>
-            <p>Grigory Perelman is the sole creator of this application. Good luck contacting him... lmao</p>
-          </article>
-      </div>
+    <h1>Meet The Team</h1>
+    <div class="aboutDisplay">
+    
+    <div>
+        <h2>TJ Willis</h2>
+        <img id="tjPic" src="${tjPic}" alt="tj-profile-img">
+        <p>TJ is a wiley software developer with a passion for excellence and an obsession with perfection. He has been with the JAC team for roughly 2 weeks and has catapulted himself to President of Youth Education. A truely elite ascension</p>
+    </div>
+    <div>
+        <h2>Matt Welfley</h2>
+        <img id="mattPic" src="${mattPic}" alt="matt-profile-img">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero nibh, cursus at eleifend a, sodales sit amet magna. Duis convallis orci venenatis nisl iaculis elementum. Nunc tempor nibh elit, id volutpat mi tempus tempor.</p>
+    </div>
+    <div>
+        <h2>Thomas Fountain</h2>
+        <img id="tommyPic" src="${tommyPic}" alt="tommy-profile-img">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero nibh, cursus at eleifend a, sodales sit amet magna. Duis convallis orci venenatis nisl iaculis elementum. Nunc tempor nibh elit, id volutpat mi tempus tempor.</p>
+    </div>
+    <div>
+        <h2>Landon Sims</h2>
+        <img id="landonPic" src="${landonPic}" alt="landon-profile-img">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero nibh, cursus at eleifend a, sodales sit amet magna. Duis convallis orci venenatis nisl iaculis elementum. Nunc tempor nibh elit, id volutpat mi tempus tempor.</p>
+    </div>
+    
+    
+    <div>
+        <h2>Aaron Dormer</h2>
+        <img id="aaronPic" src="${aaronPic}" alt="aaron-profile-img">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi libero nibh, cursus at eleifend a, sodales sit amet magna. Duis convallis orci venenatis nisl iaculis elementum. Nunc tempor nibh elit, id volutpat mi tempus tempor.</p>
+    </div>
+  
+</div>
     </div>
       `;
 }
