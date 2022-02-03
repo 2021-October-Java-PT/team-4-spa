4cc5c88e27b0e710c9191c360e11786e21cc7a6e
Matt1218 <welfleym@gmail.com>
2022-02-02 20:36:14 -0500

    fixed recipe.js to show type of meal

--- client-side/js/components/Recipe.js
+++ client-side/js/components/Recipe.js
@@ -3,7 +3,7 @@ export default function Recipe(recipe){
     <h1>${recipe.dish}</h1>
     <h2>${recipe.ingredients}</h2>
     <h3>${recipe.directions}</h3>
-    
+    <h3>${recipe.meal.timeOfDay}</h3>
 `;
 }
     
