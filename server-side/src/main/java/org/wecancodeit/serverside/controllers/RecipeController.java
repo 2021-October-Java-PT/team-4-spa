caca4a7f565e900a131dff4bd80939e18f3106b2
Matt1218 <welfleym@gmail.com>
2022-02-03 17:07:09 -0500

    button to submit rating but needs to show rating on recipe

--- server-side/src/main/java/org/wecancodeit/serverside/controllers/RecipeController.java
+++ server-side/src/main/java/org/wecancodeit/serverside/controllers/RecipeController.java
@@ -4,8 +4,10 @@ package org.wecancodeit.serverside.controllers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+import org.wecancodeit.serverside.models.Rating;
 import org.wecancodeit.serverside.models.Recipe;
 import org.wecancodeit.serverside.repositories.RecipeRepository;
+import org.wecancodeit.serverside.repositories.RatingRepository;
 
 import javax.annotation.Resource;
 import javax.swing.text.html.Option;
@@ -19,6 +21,9 @@ public class RecipeController {
     @Resource
     private RecipeRepository recipeRepo;
 
+    @Resource
+    private RatingRepository ratingRepo;
+
     @GetMapping("/api/recipes")
     public Collection<Recipe> getRecipes() {
         return (Collection<Recipe>) recipeRepo.findAll();
@@ -26,6 +31,20 @@ public class RecipeController {
 
     @GetMapping("/api/recipes/{id}")
     public Optional<Recipe> getRecipe(@PathVariable Long id) {
+        return recipeRepo.findById(id);
+    }
+
+    @PostMapping("/api/recipes/{id}/add-rating")
+    public Optional<Recipe> addRatingToRecipe(@RequestBody String body, @PathVariable Long id) throws JSONException{
+        JSONObject newRating = new JSONObject(body);
+        String ratingName = newRating.getString("ratingName");
+        Optional<Rating> ratingToAddOpt = ratingRepo.findByName(ratingName);
+        if (ratingToAddOpt.isPresent()){
+            Optional<Recipe> recipeToAddRatingToOpt = recipeRepo.findById(id);
+            Recipe recipeToAddRatingTo = recipeToAddRatingToOpt.get();
+            recipeToAddRatingTo.addRating(ratingToAddOpt.get());
+            recipeRepo.save(recipeToAddRatingTo);
+        }
         return recipeRepo.findById(id);
     }
 
--- server-side/src/main/java/org/wecancodeit/serverside/models/Rating.java
+++ server-side/src/main/java/org/wecancodeit/serverside/models/Rating.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 @Entity
 public class Rating {
@@ -36,5 +37,17 @@ public class Rating {
     public Rating(String name){
         this.name = name;
         recipes = new ArrayList<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rating rating = (Rating) o;
+        return Objects.equals(id, rating.id);
+    }
+
+    public int hashCode(){
+        return Objects.hash(id);
     }
 }
